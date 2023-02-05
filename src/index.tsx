67fab9a9b9bc47cabd36931204ad60050862d3eb
aqigif <aqilgifari0@gmail.com>
2023-02-05 20:47:29 +0700

    chore: install tailwind and material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 
 const container = document.getElementById('root')!;
