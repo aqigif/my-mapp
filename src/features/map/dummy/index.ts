78ee22ae1ad135ffef52ffe249464ab06e7f7c4b
aqigif <aqilgifari0@gmail.com>
2023-02-12 00:48:59 +0700

    test: add mapSlice unit testing

---
+++ src/features/map/dummy/index.ts
@@ -0,0 +1,661 @@
+export const dummySelectData = {
+  description: "Tesla Giga Texas, Tesla Road, Austin, Texas, Amerika Serikat",
+  structured_formatting: {
+    main_text: "Tesla Giga Texas",
+    main_text_matched_substrings: [
+      {
+        length: 5,
+        offset: 0,
+      },
+    ],
+    secondary_text: "Tesla Road, Austin, Texas, Amerika Serikat",
+  },
+  place_id: "ChIJJedYjPy3RIYR6i3mJif1om8",
+  location: {
+    lat: 30.2231449,
+    lng: -97.61858,
+  },
+  detail: {
+    address_components: [
+      {
+        long_name: "13101",
+        short_name: "13101",
+        types: ["street_number"],
+      },
+      {
+        long_name: "Tesla Road",
+        short_name: "Tesla Rd",
+        types: ["route"],
+      },
+      {
+        long_name: "Austin",
+        short_name: "Austin",
+        types: ["locality", "political"],
+      },
+      {
+        long_name: "Travis County",
+        short_name: "Travis County",
+        types: ["administrative_area_level_2", "political"],
+      },
+      {
+        long_name: "Texas",
+        short_name: "TX",
+        types: ["administrative_area_level_1", "political"],
+      },
+      {
+        long_name: "Amerika Serikat",
+        short_name: "US",
+        types: ["country", "political"],
+      },
+      {
+        long_name: "78725",
+        short_name: "78725",
+        types: ["postal_code"],
+      },
+      {
+        long_name: "5712",
+        short_name: "5712",
+        types: ["postal_code_suffix"],
+      },
+    ],
+    adr_address:
+      '<span class="street-address">13101 Tesla Rd</span>, <span class="locality">Austin</span>, <span class="region">TX</span> <span class="postal-code">78725-5712</span>, <span class="country-name">Amerika Serikat</span>',
+    business_status: "OPERATIONAL",
+    current_opening_hours: {
+      open_now: true,
+      periods: [
+        {
+          close: {
+            date: "2023-02-12",
+            day: 0,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-12",
+            day: 0,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-13",
+            day: 1,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-13",
+            day: 1,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-14",
+            day: 2,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-14",
+            day: 2,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-15",
+            day: 3,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-15",
+            day: 3,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-16",
+            day: 4,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-16",
+            day: 4,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-17",
+            day: 5,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-17",
+            day: 5,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            date: "2023-02-11",
+            day: 6,
+            time: "2330",
+          },
+          open: {
+            date: "2023-02-11",
+            day: 6,
+            time: "0600",
+          },
+        },
+      ],
+      weekday_text: [
+        "Senin: 06.00–23.30",
+        "Selasa: 06.00–23.30",
+        "Rabu: 06.00–23.30",
+        "Kamis: 06.00–23.30",
+        "Jumat: 06.00–23.30",
+        "Sabtu: 06.00–23.30",
+        "Minggu: 06.00–23.30",
+      ],
+    },
+    formatted_address: "13101 Tesla Rd, Austin, TX 78725, Amerika Serikat",
+    geometry: {
+      location: {
+        lat: 30.2231449,
+        lng: -97.61858,
+      },
+      viewport: {
+        northeast: {
+          lat: 30.2244790302915,
+          lng: -97.61578745000001,
+        },
+        southwest: {
+          lat: 30.2217810697085,
+          lng: -97.61951084999998,
+        },
+      },
+    },
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/generic_business-71.png",
+    icon_background_color: "#7B9EB0",
+    icon_mask_base_uri:
+      "https://maps.gstatic.com/mapfiles/place_api/icons/v2/generic_pinlet",
+    name: "Tesla Giga Texas",
+    opening_hours: {
+      open_now: true,
+      periods: [
+        {
+          close: {
+            day: 0,
+            time: "2330",
+          },
+          open: {
+            day: 0,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 1,
+            time: "2330",
+          },
+          open: {
+            day: 1,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 2,
+            time: "2330",
+          },
+          open: {
+            day: 2,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 3,
+            time: "2330",
+          },
+          open: {
+            day: 3,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 4,
+            time: "2330",
+          },
+          open: {
+            day: 4,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 5,
+            time: "2330",
+          },
+          open: {
+            day: 5,
+            time: "0600",
+          },
+        },
+        {
+          close: {
+            day: 6,
+            time: "2330",
+          },
+          open: {
+            day: 6,
+            time: "0600",
+          },
+        },
+      ],
+      weekday_text: [
+        "Senin: 06.00–23.30",
+        "Selasa: 06.00–23.30",
+        "Rabu: 06.00–23.30",
+        "Kamis: 06.00–23.30",
+        "Jumat: 06.00–23.30",
+        "Sabtu: 06.00–23.30",
+        "Minggu: 06.00–23.30",
+      ],
+    },
+    photos: [
+      {
+        height: 9248,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/115959696501720554717">Kay.K.J. Han</a>',
+        ],
+        photo_reference:
+          "AfLeUgM2-frahq7WbUc_cBg-_jTB4TIuyOnWN2S9v6r_wXOUO7c49lpNApaFhNnDY5IYrymJpfv43RHZ_qdY-7WlFuhcQUwXjyqMwktEFtlqubFk7OttvEllmCl6SRZei6zzhj-RHhR5Hfbx8uE1RB2J2N4uSyZMOMf5W2fBa4xEyQ-f4eiS",
+        width: 6936,
+      },
+      {
+        height: 2873,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/115663757806817092000">SpaceXR Academy</a>',
+        ],
+        photo_reference:
+          "AfLeUgODe4iPjhK0PQb_6m0WyD8jrmlmkFo8Lt5_-49qQd8Sp9xX6h9_GwtENO0MLL788qU7KZq2l6L-SMqyJ8DHVIMA_GZxT2pwDG-xbXbK5A1PXe0b86dJPiNhVghKlxrJkgT_legzIEQN6wxM1yP0GTllVk8jOjWfVMH8dbSwuoJMZPNG",
+        width: 2873,
+      },
+      {
+        height: 1080,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/105464310199107035338">Kesa Maza</a>',
+        ],
+        photo_reference:
+          "AfLeUgPd_hp0T1PEctQEjW5ruUaE20k48aO2Ql27ZoVyslzIrnpZjOnccq39D5MZE42Nls6r27DOuCUYM99HUJSlytNTCOCCSFempKcCpAnzuVlCxDNUZOVktuvklsmZq1dTfBEF7e3SLedIKqC5Sb1HgsVZWCqKG_xf4OkboePnILCj_HcD",
+        width: 1920,
+      },
+      {
+        height: 1200,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/115411553724310627198">Rishi Roy</a>',
+        ],
+        photo_reference:
+          "AfLeUgNGE32QQJxuZk4Kyg5E1TZ_ZVGHvIth9U5kxzyj0N0qSCN98wDs3bGSeT7hBkZnbs8mpY9um1_LlXXHPunD9q9TgNcFUmj02n1HbcE4sWDOmtkSAzoVN7_1sscjM28HrHz9QtvruGeiO5tf8jul7Kj0OmMVFLKx9rqX30oRSM-f_-Av",
+        width: 1600,
+      },
+      {
+        height: 3072,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/111442190230847869047">Franklin C</a>',
+        ],
+        photo_reference:
+          "AfLeUgNy3vtOf3gXZqcvh3uos9IpRW_mLR53ywNkEV36hHkSyESMPbY8atUx_JPDAgskjdICap11-Fkx69LEzIVcUjuF_Q9tatknqFEWOSoxdQOGpJyRXGwHZhX920nY-tIXjHpKfzZVemlUuJ7qwxMKoFkXdcU9cSWFzsabSBkPxAL8FtAz",
+        width: 4080,
+      },
+      {
+        height: 6936,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/117306084588744780451">Spoiled Bread</a>',
+        ],
+        photo_reference:
+          "AfLeUgN1dPlXPkHherGz07lv5Iy6TKGgKmJnB1IEA7jt-YlqWDA46gIKVotJY9IRwNJ7Pg2m1lQhFgktmXLVBb42MU4afpM3IEWn5Vi77xQoWprQCSvtFB05YE7dLTxVuOgrVOJfkrbPN9L__2se9Y6Hjaw-8fdHjs8US_mS2USyGt0UNscf",
+        width: 9248,
+      },
+      {
+        height: 1800,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/116795573276363427824">The Last Samurai</a>',
+        ],
+        photo_reference:
+          "AfLeUgOW-uwEg7RGO_YuDh248-2JDE79UPDoDCg719JkZtTq6bbnGSNWldm8P2qO0szR39QOS4SPbDYCa-2MR6MCLUCZWHCkt__qJOFmvhkk5Y-CqkUYdFAuTJdWZ0zh53HZGf4THHLlprijJIVyvtTISvLSb3MnXMvfrSEtJTaGnZm_sQLJ",
+        width: 2880,
+      },
+      {
+        height: 3024,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/117161923610313390862">D 2</a>',
+        ],
+        photo_reference:
+          "AfLeUgO6_BvGsWPDkmISJStlgmshpfgnPhsGohV805Eny_Ywok8nvi5M4DtqnMxQTaDZ1R-oNqOiCobwF_NCehjjE7nSPnQ8vNctdKJVNZIJqba3zIB9PWhjrr3mNwrd2SL6Lo7d6MIUJiVbVB1mNjtrL69NDqBrLfru6e_3QRp9XT_T-ESr",
+        width: 4032,
+      },
+      {
+        height: 2250,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/106042769271712458995">Joe Tegtmeyer</a>',
+        ],
+        photo_reference:
+          "AfLeUgMiHuHAdglRZHJ_hSG8rKAeZZokaGwQrcFORP7KV-w81oZNBvmZ8kXgpsGMcJsNT_8kHS2pnL1SZPUkY0STwyYbRS1BRiRabAMOCCXtand31p6PXDQEqFwvVsavF43TS4UGCbpyj5LniHMCOuA2UK6iq8A2v-5my7kkU7fpJ0AdYn9l",
+        width: 4000,
+      },
+      {
+        height: 3024,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/114160663669917286331">Miguel Arturo Sierra Zàrate</a>',
+        ],
+        photo_reference:
+          "AfLeUgNCCG3MJDAADzQyxe2JvE0AuJvaW-9Z4Bm6a1IYzC5hdrEci_43lP-3PyJpWGqg19Zxvu2fJMelHD_XcP_9wPlcInk_J8wB6t74nFgM0j1tGUv-bAx6X7CP8mIS8_Orrktxi93DTahQH4tdkgBCaRprkOUpxj1YELljV6L5H72RVqM",
+        width: 4032,
+      },
+    ],
+    place_id: "ChIJJedYjPy3RIYR6i3mJif1om8",
+    plus_code: {
+      compound_code: "69FJ+7H Austin, Texas, Amerika Serikat",
+      global_code: "862469FJ+7H",
+    },
+    rating: 4.5,
+    reference: "ChIJJedYjPy3RIYR6i3mJif1om8",
+    reviews: [
+      {
+        author_name: "Amina A",
+        author_url:
+          "https://www.google.com/maps/contrib/109180872984619145773/reviews",
+        language: "id",
+        original_language: "en",
+        profile_photo_url:
+          "https://lh3.googleusercontent.com/a-/AD5-WCkVR8GpzWGB0vB-acQ6ElUgD5-P5lXZ2dD7l2iK4Q=s128-c0x00000000-cc-rp-mo-ba3",
+        rating: 5,
+        relative_time_description: "9 bulan lalu",
+        text: "Mengunjungi tempat ini selama Cyber ​​Rodeo! Itu adalah ledakan! Tempat ini sangat besar! Mereka mengatur musik live, truk makanan, kebun binatang dan lain-lain! Staf benar-benar luar biasa! Sangat ramah dan sangat membantu jika ada petunjuk yang diperlukan! Akan sangat menyenangkan untuk kembali ke sini untuk acara besar.",
+        time: 1651521852,
+        translated: true,
+      },
+      {
+        author_name: "coolzpl",
+        author_url:
+          "https://www.google.com/maps/contrib/112919263971945479163/reviews",
+        language: "id",
+        original_language: "en",
+        profile_photo_url:
+          "https://lh3.googleusercontent.com/a-/AD5-WCmOtNSAa3h7v8j3ANv6n5ERexiYUvqNBaNrrvHKWA=s128-c0x00000000-cc-rp-mo-ba3",
+        rating: 5,
+        relative_time_description: "8 bulan lalu",
+        text: "Saya telah bekerja di sini selama lebih dari setahun. Beberapa hal yang sangat luar biasa terjadi di sini. Saya bekerja dengan beberapa orang paling berbakat yang pernah bekerja dengan saya. Beberapa hal paling keren yang pernah saya lihat akan segera keluar dari fasilitas ini di tahun-tahun mendatang. Dari pria RV.",
+        time: 1654204091,
+        translated: true,
+      },
+      {
+        author_name: "Morten Christiansen",
+        author_url:
+          "https://www.google.com/maps/contrib/106929755532951246061/reviews",
+        language: "id",
+        original_language: "da",
+        profile_photo_url:
+          "https://lh3.googleusercontent.com/a-/AD5-WCk1c0NakEJ9vIF2upGYM_-dmxB8gMuqVaa1mUav=s128-c0x00000000-cc-rp-mo",
+        rating: 3,
+        relative_time_description: "9 bulan lalu",
+        text: "Tamu tidak diterima. Kami datang untuk melihat pabrik baru, kami dibuang oleh penjaga sebelum kami berhasil mengemudi ke tempat parkir. Kami juga tidak diterima di acara Demo mereka karena kami tidak ada dalam daftar tamu. Bila datang dari jalan tol ada rambu-rambu dengan tulisan pengunjung, tapi Anda tidak bisa datang begitu saja, membutuhkan undangan. Namun dilihat dari luar, ini adalah bangunan yang sangat besar dan baru.",
+        time: 1650593091,
+        translated: true,
+      },
+      {
+        author_name: "Ace Constantine",
+        author_url:
+          "https://www.google.com/maps/contrib/105480200500205351900/reviews",
+        language: "id",
+        original_language: "en",
+        profile_photo_url:
+          "https://lh3.googleusercontent.com/a-/AD5-WCkQR9ArGsAjPDOnyjRJZXPmWae_xOPecXC1g3Ftj6s=s128-c0x00000000-cc-rp-mo-ba4",
+        rating: 5,
+        relative_time_description: "10 bulan lalu",
+        text: "Tempat yang bagus untuk pesta. Mungkin perluas keseluruhan acara menjadi beberapa hari sehingga semua yang menginginkan tiket bisa mendapatkan tiket. Hebatnya 15k tidak cukup.",
+        time: 1649519866,
+        translated: true,
+      },
+      {
+        author_name: "Jeff Roberts",
+        author_url:
+          "https://www.google.com/maps/contrib/109389994545307061850/reviews",
+        language: "id",
+        original_language: "en",
+        profile_photo_url:
+          "https://lh3.googleusercontent.com/a-/AD5-WCl9qTcM_bx7kTqisVxlKzJp3oiDKg7ogMdxxOHr=s128-c0x00000000-cc-rp-mo-ba3",
+        rating: 5,
+        relative_time_description: "setahun yang lalu",
+        text: "Giga Texas adalah pencapaian luar biasa dan pencapaian keinginan manusia untuk menjadi manusia yang lebih baik di Bumi dan akhirnya di Mars! Saya mengunjungi hampir setiap hari dan situs ini lebih mengesankan setiap kali seperti halnya orang-orangnya (kru terbaik di planet ini)!",
+        time: 1643219347,
+        translated: true,
+      },
+    ],
+    types: ["point_of_interest", "establishment"],
+    url: "https://maps.google.com/?cid=8044261432942276074",
+    user_ratings_total: 867,
+    utc_offset: -360,
+    vicinity: "13101 Tesla Road, Austin",
+    website: "http://tesla.com/",
+    wheelchair_accessible_entrance: true,
+  },
+};
+
+export const dummySearchOptions = [
+  {
+    description: "Tesla Giga Texas, Tesla Road, Austin, Texas, Amerika Serikat",
+    matched_substrings: [
+      {
+        length: 3,
+        offset: 0,
+      },
+    ],
+    place_id: "ChIJJedYjPy3RIYR6i3mJif1om8",
+    reference: "ChIJJedYjPy3RIYR6i3mJif1om8",
+    structured_formatting: {
+      main_text: "Tesla Giga Texas",
+      main_text_matched_substrings: [
+        {
+          length: 3,
+          offset: 0,
+        },
+      ],
+      secondary_text: "Tesla Road, Austin, Texas, Amerika Serikat",
+    },
+    terms: [
+      {
+        offset: 0,
+        value: "Tesla Giga Texas",
+      },
+      {
+        offset: 18,
+        value: "Tesla Road",
+      },
+      {
+        offset: 30,
+        value: "Austin",
+      },
+      {
+        offset: 38,
+        value: "Texas",
+      },
+      {
+        offset: 45,
+        value: "Amerika Serikat",
+      },
+    ],
+    types: ["point_of_interest", "establishment"],
+  },
+  {
+    description:
+      "Tesla Gigafactory Berlin-Brandenburg, Tesla Str., Grünheide, Jerman",
+    matched_substrings: [
+      {
+        length: 3,
+        offset: 0,
+      },
+    ],
+    place_id: "ChIJ51B0_1g7qEcRAAs8N5sgtNo",
+    reference: "ChIJ51B0_1g7qEcRAAs8N5sgtNo",
+    structured_formatting: {
+      main_text: "Tesla Gigafactory Berlin-Brandenburg",
+      main_text_matched_substrings: [
+        {
+          length: 3,
+          offset: 0,
+        },
+      ],
+      secondary_text: "Tesla Str., Grünheide, Jerman",
+    },
+    terms: [
+      {
+        offset: 0,
+        value: "Tesla Gigafactory Berlin-Brandenburg",
+      },
+      {
+        offset: 38,
+        value: "Tesla Str.",
+      },
+      {
+        offset: 50,
+        value: "Grünheide",
+      },
+      {
+        offset: 61,
+        value: "Jerman",
+      },
+    ],
+    types: ["point_of_interest", "establishment"],
+  },
+  {
+    description:
+      "Tesla, Fremont Boulevard, Fremont, California, Amerika Serikat",
+    matched_substrings: [
+      {
+        length: 3,
+        offset: 0,
+      },
+    ],
+    place_id: "ChIJSb979lTGj4ARJftHPERsTy0",
+    reference: "ChIJSb979lTGj4ARJftHPERsTy0",
+    structured_formatting: {
+      main_text: "Tesla",
+      main_text_matched_substrings: [
+        {
+          length: 3,
+          offset: 0,
+        },
+      ],
+      secondary_text: "Fremont Boulevard, Fremont, California, Amerika Serikat",
+    },
+    terms: [
+      {
+        offset: 0,
+        value: "Tesla",
+      },
+      {
+        offset: 7,
+        value: "Fremont Boulevard",
+      },
+      {
+        offset: 26,
+        value: "Fremont",
+      },
+      {
+        offset: 35,
+        value: "California",
+      },
+      {
+        offset: 47,
+        value: "Amerika Serikat",
+      },
+    ],
+    types: ["car_dealer", "point_of_interest", "store", "establishment"],
+  },
+  {
+    description: "Tesero, Trentino, Italia",
+    matched_substrings: [
+      {
+        length: 3,
+        offset: 0,
+      },
+    ],
+    place_id: "ChIJ0Sp6wNJ8eEcRoGv_r333Drg",
+    reference: "ChIJ0Sp6wNJ8eEcRoGv_r333Drg",
+    structured_formatting: {
+      main_text: "Tesero",
+      main_text_matched_substrings: [
+        {
+          length: 3,
+          offset: 0,
+        },
+      ],
+      secondary_text: "Trentino, Italia",
+    },
+    terms: [
+      {
+        offset: 0,
+        value: "Tesero",
+      },
+      {
+        offset: 8,
+        value: "Trentino",
+      },
+      {
+        offset: 18,
+        value: "Italia",
+      },
+    ],
+    types: ["locality", "political", "geocode"],
+  },
+  {
+    description:
+      "Tesla Giga Nevada, Electric Avenue, Sparks, Nevada, Amerika Serikat",
+    matched_substrings: [
+      {
+        length: 3,
+        offset: 0,
+      },
+    ],
+    place_id: "ChIJuTC6QMIfmYAR2FzlT_qwZn4",
+    reference: "ChIJuTC6QMIfmYAR2FzlT_qwZn4",
+    structured_formatting: {
+      main_text: "Tesla Giga Nevada",
+      main_text_matched_substrings: [
+        {
+          length: 3,
+          offset: 0,
+        },
+      ],
+      secondary_text: "Electric Avenue, Sparks, Nevada, Amerika Serikat",
+    },
+    terms: [
+      {
+        offset: 0,
+        value: "Tesla Giga Nevada",
+      },
+      {
+        offset: 19,
+        value: "Electric Avenue",
+      },
+      {
+        offset: 36,
+        value: "Sparks",
+      },
+      {
+        offset: 44,
+        value: "Nevada",
+      },
+      {
+        offset: 52,
+        value: "Amerika Serikat",
+      },
+    ],
+    types: ["point_of_interest", "establishment"],
+  },
+];
--- src/features/map/mapSlice.spec.ts
+++ src/features/map/mapSlice.spec.ts
@@ -1,19 +1,47 @@
-import { } from './mapSlice';
+import { dummySearchOptions, dummySelectData } from "./dummy";
+import mapSlice, { actionGetOptions, actionReset, actionSearch, actionSelectPlace } from "./mapSlice";
+import { MapState } from "./types/mapTypes";
 
-describe('map reducer', () => {
-  // const initialState: MapState = {
-  //   selectedPlace: null
-  // };
-  // it('should handle initial state', () => {
-  //   expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
-  //     selectedPlace: null,
-  //   });
-  // });
-
-  // it('should handle select place', () => {
-  //   const actual = counterReducer(initialState, selectPlace({
-      
-  //   }));
-  //   expect(actual.selectedPlace).toEqual(null);
-  // });
+describe("map reducer", () => {
+  const initialState: MapState = {
+    selectedPlace: null,
+    searchOptions: [],
+    searchText: "",
+    searchValue: null,
+  };
+  it("should handle initial state", () => {
+    expect(mapSlice(undefined, { type: "unknown" })).toEqual({
+      selectedPlace: initialState.selectedPlace,
+      searchOptions: initialState.searchOptions,
+      searchText: initialState.searchText,
+      searchValue: initialState.searchValue,
+    });
+  });
+  it("should handle action Select Place", () => {
+    const actual = mapSlice(initialState, actionSelectPlace(dummySelectData));
+    expect(actual.selectedPlace).toEqual(dummySelectData);
+    expect(actual.searchValue).toEqual({
+      description: dummySelectData.description,
+      structured_formatting: dummySelectData.structured_formatting,
+      place_id: dummySelectData.place_id,
+    });
+    expect(actual.searchText).toEqual(
+      dummySelectData.structured_formatting.main_text
+    );
+  });
+  it("should handle action Search Options", () => {
+    const actual = mapSlice(initialState, actionGetOptions(dummySearchOptions));
+    expect(actual.searchOptions).toEqual(dummySearchOptions);
+  });
+  it("should handle action Search Text", () => {
+    const actual = mapSlice(initialState, actionSearch("Tesl"));
+    expect(actual.searchText).toEqual('Tesl');
+  });
+  it("should handle action Reset", () => {
+    const actual = mapSlice(initialState, actionReset());
+    expect(actual.searchText).toEqual('');
+    expect(actual.searchOptions).toEqual([]);
+    expect(actual.searchValue).toEqual(null);
+    expect(actual.selectedPlace).toEqual(null);
+  });
 });
